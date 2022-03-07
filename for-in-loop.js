a9e7061b9763e6ca3791c8df69d946b6716a7f90
shantoe <shantoroy1792@gmail.com>
2022-03-07 13:14:04 +0600

    Second comment

--- for-in-loop.js
+++ for-in-loop.js
@@ -1,7 +1,11 @@
 // for in loop use in object 
 const person = {fname: 'shanto', lname: "roy", age: 25};
 for(let x in person){
+  // get here property
   console.log(x);
   // x variable ar value ka dorta hola array syntax use korta hoba
   console.log(person[x]);
 }
+
+
+// Do not use for in over an Array if the index order is important.
